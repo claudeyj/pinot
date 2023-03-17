a47f50918af0802d5514421175674f88bc0dae3e
Jun Yang <jy70@illinois.edu>
2023-03-17 12:59:56 -0500

    add workflow for running nondex

---
+++ .github/workflows/scripts/.nondex.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+# Java version
+java -version
+
+mvn clean install -pl pinot-core -am -DskipTests || exit 1
+
+# Run test without shuffling
+echo "Running test without shuffling"
+mvn -pl pinot-core test -Dtest=org.apache.pinot.core.util.CrcUtilsTest#test1 || exit 1
+
+# Run nondex
+echo "Running nondex"
+mvn -pl pinot-core edu.illinois:nondex-maven-plugin:2.1.1:nondex -Dtest=org.apache.pinot.core.util.CrcUtilsTest#test1 && exit 0 || exit 1
